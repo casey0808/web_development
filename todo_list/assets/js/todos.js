49526e471b7a0fb48762c98db5846acbe6634082
casey0808 <casey_c0808@outlook.com>
2019-11-17 10:36:03 +0800

    update

--- todo_list/assets/js/todos.js
+++ todo_list/assets/js/todos.js
@@ -15,7 +15,7 @@ $("input[type='text']").keypress(function(event){
     if(event.which === 13){
         var todoText = $(this).val();
         $(this).val = "";
-        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
     };
 })
 
